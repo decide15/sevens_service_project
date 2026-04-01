aa0c2926fd2a1f35193c46ff34e244132561fcdf
Seven <decide15@163.com>
2026-04-01 16:50:44 +0800

    重启脚本

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "🚀 Pull latest code..."
+git pull
+
+echo "📦 Install dependencies..."
+npm install
+
+echo "🔄 Restart service..."
+pm2 restart sevens_service_project || pm2 start server.js --name sevens_service_project
+
+echo "✅ Deploy done!"
