a30b1afefcb5e7881a45feae3f353f94cef97b1c
xiaoqi <xiaoqi@yazhai.co>
2026-04-01 18:30:55 +0800

    重启

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ echo "📦 Install dependencies..."
 npm install
 
 echo "🔄 Restart service..."
-pm2 restart sevens_service_project || pm2 start server.js --name sevens_service_project
+pm2 restart sevens_service_project --update-env || pm2 start server.js --name sevens_service_project
 
 echo "✅ Deploy done!"
